73832a8be0e21e6313e2bf16d3971db7b9bbf0dd
OhWhale515 <sterlinggreen515@gmail.com>
2023-02-07 13:28:54 -0500

    player/enemy velocity set up. canvas h,w adjust

--- Game-Content/index.js
+++ Game-Content/index.js
@@ -10,15 +10,20 @@ class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
+        this.height = 150
     }
     draw() {
         c.fillStyle = 'green'
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
     }
-    update(){
+    update() {
         this.draw()
-        this.position.y += 10
-    }
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        }
+    }  
 }
 
 
@@ -29,7 +34,7 @@ const player = new Sprite({
     },
     velocity:{
         x: 0,
-        y: 0
+        y: 10
     }
 })
 
